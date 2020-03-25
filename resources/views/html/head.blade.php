36f634e6dd5525c5235839377b6d58cde197084b
sonthth1903012 <sonthth1903012@fpt.edu.vn>
2020-03-25 16:12:31 +0700

    123

--- resources/views/html/head.blade.php
+++ resources/views/html/head.blade.php
@@ -1,5 +1,13 @@
 <head>
     <title>T1904A - @yield("title")</title>
+
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="manifest" href="site.webmanifest">
+    <link rel="shortcut icon" type="image/x-icon" href="assets/img/favicon.ico">
     <!-- CSS here -->
     <link rel="stylesheet" href={{("assets/css/bootstrap.min.css")}}>
     <link rel="stylesheet" href={{("assets/css/owl.carousel.min.css")}}>
