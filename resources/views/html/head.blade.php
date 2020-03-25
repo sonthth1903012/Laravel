d26df36a22a5f14680c64c47c109359c4d51653f
sonthth1903012 <sonthth1903012@fpt.edu.vn>
2020-03-25 16:38:33 +0700

    Update head.blade.php

--- resources/views/html/head.blade.php
+++ resources/views/html/head.blade.php
@@ -22,22 +22,6 @@
     <link rel="stylesheet" href={{("assets/css/style.css")}}>
     <link rel="stylesheet" href={{("css/style.css")}}/>
 
-    <link href="img/favicon.ico" rel="shortcut icon"/>
-
-    <!-- Google Fonts -->
-    <link href="https://fonts.googleapis.com/css?family=Raleway:400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
-
-    <!-- Stylesheets -->
-    <link rel="stylesheet" href="css/bootstrap.min.css"/>
-    <link rel="stylesheet" href="css/font-awesome.min.css"/>
-    <link rel="stylesheet" href="css/owl.carousel.css"/>
-    <link rel="stylesheet" href="css/style.css"/>
-    <link rel="stylesheet" href="css/animate.css"/>
-
-
-    <!--[if lt IE 9]>
-    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
+    
 
 </head>
